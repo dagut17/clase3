fd50290ea8bf59d7242937a7c95082a8266ab46a
dagut17 <dagutierrez17@gmail.com>
2016-02-01 19:39:31 -0600

    version1.0
    Consola

---
+++ clase3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase3
+{
+    public class Person
+    {
+        public string name;
+
+        public Person()
+        {
+            name = "unknown";
+        }
+        public Person(string nm)
+        {
+            name = nm;
+        }
+        public void Setname(string newName)
+        {
+            name = newName;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Person person1 = new Person();
+                Console.WriteLine(person1.name);
+
+                person1.Setname("Daniel Gutiérrez");
+                Console.WriteLine(person1.name);
+
+                Person person2 = new Person("Desconocido");
+                Console.WriteLine(person2.name);
+
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
+        
+        
+    
+
